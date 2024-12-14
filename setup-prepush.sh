2e3158226af69a1083469d4a0e0851dc9d65b94e
2814109 <2814109k@gmail.com>
2024-12-15 02:48:59 +0900

    chore

---
+++ setup-prepush.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Move to the .git/hooks directory
+cd .git/hooks
+
+# Create the pre-push hook file
+touch pre-push
+
+# Add the npm command to the pre-push file
+echo -e "#!/bin/sh\n\nmake run deploy" > pre-push
+
+# Make the pre-push file executable
+chmod +x pre-push
+
+# Confirm success
+echo "pre-push hook created and made executable."
