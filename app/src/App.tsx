a13965bb9cfa0e69b41bd7f62fde454efbecfe29
2814109 <2814109k@gmail.com>
2024-12-15 22:29:15 +0900

    chore

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,21 @@
-
+import { AppSidebar } from "@/components/Sidebar";
 import { AddFriendForm } from "./components/AddFriendForm";
 import { FriendList } from "./components/FriendList";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 function App() {
-
   return (
     <>
-    <h1>My simple Dexie app</h1>
-
-    <h2>Add Friend</h2>
-    <AddFriendForm defaultAge={21} />
-
-    <h2>Friend List</h2>
-    <FriendList minAge={18} maxAge={65} />
-  </>
+      <AppSidebar />
+      <SidebarTrigger size={"lg"} />
+      <div>
+        <h1>My simple Dexie app</h1>
+        <h2>Add Friend</h2>
+        <AddFriendForm defaultAge={21} />
+        <h2>Friend List</h2>
+        <FriendList minAge={18} maxAge={65} />
+      </div>
+    </>
   );
 }
 
