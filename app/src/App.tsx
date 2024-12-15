e7ffb094cf01ecfe3d4ae68f99f8efc6ce10eb87
2814109 <2814109k@gmail.com>
2024-12-16 02:23:54 +0900

    chore

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <div className="flex w-screen h-screen bg-background text-foreground">
       <main className="flex-1 flex flex-col overflow-scroll">
-        <header className=" py-4 px-4 bg-primary text-primary-foreground">
+        <header className=" py-4 px-4">
           <section className="container flex justify-between items-center mx-auto px-4 sm:px-8">
             <h1 className="text-xl font-bold whitespace-nowrap font-serif">
               Taming The Number
