1f236f2ef38ddb70d6f1a8aef0ee02362ffa9222
2814109 <2814109k@gmail.com>
2024-12-21 18:06:39 +0900

    chore

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/taming-the-numbers/",
+  base: "/srv-vinci/",
 });
